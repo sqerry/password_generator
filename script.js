b78becf0824a693f8c83aaeae937e2976b9158fe
Robert Nguyen <rn@antstudio.cz>
2022-01-26 23:39:56 +0100

    refactor js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const passwordGenerator = () =>{
+const passwordGenerator = () => {
     const resultEl = document.getElementById('result');
     const lengthEl = document.getElementById('length');
     const uppercaseEl = document.getElementById('uppercase');
@@ -7,106 +7,90 @@ const passwordGenerator = () =>{
     const symbolsEl = document.getElementById('symbols');
     const generateEl = document.getElementById('generate');
     const clipboard = document.getElementById('clipboard');
-
     const randomFunc = {
-        lower: getRandomLower,
-        upper: getRandomUpper,
-        number: getRandomNumber,
-        symbol: getRandomSymbol
+        lower: getRandomLower = () => {
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        },
+        upper: getRandomUpper = () => {
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        },
+        number: getRandomNumber = () => {
+            return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+        },
+        symbol: getRandomSymbol = () => {
+            const symbols = '!@#$%^&*(){}[]=<>/,.'
+            return symbols[Math.floor(Math.random() * symbols.length)];
+        }
     }
-
     clipboard.addEventListener('click', () => {
         const textarea = document.createElement('textarea');
         const password = resultEl.innerText;
         const checkmark = document.querySelector(".checkmark");
-
-        if(password){
+        if(password) {
             textarea.value = password;
             document.body.appendChild(textarea);
             textarea.select();
             document.execCommand('copy');
             textarea.remove();
-
-            if(checkmark){
+            if(checkmark) {
                 clipboard.classList.add("hide");
                 checkmark.classList.add("checked");
             }
         }
     });
-
     generateEl.addEventListener('click', () => {
         const checkboxes = document.querySelectorAll(".form-group input");
-
-        if(checkboxes){
+        if(checkboxes) {
             const array = [];
             array.push(lowercaseEl, uppercaseEl, numbersEl, symbolsEl);
-            let result = array.every(e  => !e.checked);
-
-            if(result){
-                for (const checkbox of checkboxes) {
+            let result = array.every(e => !e.checked);
+            if(result) {
+                for(const checkbox of checkboxes) {
                     checkbox.checked = true
                 }
             }
         }
-
         let length = +lengthEl.value;
         const hasLower = lowercaseEl.checked;
         const hasUpper = uppercaseEl.checked;
         const hasNumber = numbersEl.checked;
         const hasSymbol = symbolsEl.checked;
         const checkmark = document.querySelector(".checkmark.checked");
-
-        if(lengthEl.value < 4){
+        if(lengthEl.value < 4) {
             length = 4;
         }
-        if(clipboard.classList.contains("hide")){
+        if(clipboard.classList.contains("hide")) {
             clipboard.classList.remove("hide");
         }
-        if(checkmark){
+        if(checkmark) {
             checkmark.classList.remove("checked")
         }
-        if(resultEl){
+        if(resultEl) {
             resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
         }
     });
-
     const generatePassword = (lower, upper, number, symbol, length) => {
         let generatedPassword = '';
         const typesCount = lower + upper + number + symbol;
-        const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-
+        const typesArr = [{
+            lower
+        }, {
+            upper
+        }, {
+            number
+        }, {
+            symbol
+        }].filter(item => Object.values(item)[0]);
         if(typesCount === 0) {
             return '';
         }
-
         for(let i = 0; i < length; i += typesCount) {
             typesArr.forEach(type => {
                 const funcName = Object.keys(type)[0];
                 generatedPassword += randomFunc[funcName]();
             });
         }
-
         return generatedPassword.slice(0, length);
     }
-
-    function getRandomLower() {
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    }
-
-    function getRandomUpper() {
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    }
-
-    function getRandomNumber() {
-        return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-    }
-
-    function getRandomSymbol() {
-        const symbols = '!@#$%^&*(){}[]=<>/,.'
-        return symbols[Math.floor(Math.random() * symbols.length)];
-
-
-    }
 }
-
 passwordGenerator();
